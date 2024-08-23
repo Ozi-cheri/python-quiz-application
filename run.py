b66895d4d52e485aa4ab085985f934305a9b524d
Ozi-cheri Ihemedu <silvernasa@myyahoo.com>
2024-08-23 07:03:15 +0000

    Add the print functions,quiz title and name texts

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
+text = "Python Quiz Application"
+x = text.title()
+print(x)
+print("***********************")
+name = input("Please enter your name.")
+print(f"Hello {name}")
+print("Welcome To Python Quiz Application")
+print("Rules\nSelect from the letters A-D the correct options of each question.")
+    
+
+
+    
+
+
 
