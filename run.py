d8f48fd93dda239eff7035056fdac9b4a89560dd
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-08-30 16:21:36 +0000

    Modify quiz,option variables and run.py file

--- run.py
+++ run.py
@@ -1,54 +1,32 @@
 
 
-print("Python Quiz Application")
+print("Welcome to Python Quiz Application: ")
+player_name = input("Please enter your name.")
+print(f"Hello, {player_name}! If you are ready let's start the quiz.\n")
 
-
-print("***********************")
-name = input("Please enter your name.")
-print(f"Hello {name}")
-print("***********************")
-print("Welcome To Python Quiz Application")
-user = input("Do you want to continue?\n Yes or No\n")
-if user == "yes":
-    print("let's start the quiz")
-else:
-        print("Thanks for stopping by")
-        quit()
-print("Rules \nSelect from the letters A-D the correct option of each question.")
-print("****************************************************************")
-
-
-
-quiz_data = [
-   {   
-       "question": "What is the full meaning of PEP?",
-
-    "options": ("A) Python element particles","B. prepared elected python","C. Python pack project","D. Python enhancement proposal"),
-       "answer":"C"
-       },
-       
-       {
-        "question": "What is the full meaning of Ram?",
-       "options": ("A) Remedy after memory","B) Random acess memory","C) Release acess memory","D) Ready at memory"),
-       "answer":"B"
-       },
-
-       {"question": "What is RAM?",
-       "options": ("A) Media for coding","B) Computer disk","C) A storage medium for computers","D) A file"),
-       "answer":"C"
-       },
+quiz = {
     
-       {"question": "What will you get if you use a variable name before a variable is assigned?",
-       "options" : ("A) Function", "B) File", "C) String", "D) Error"),
-       "answer":"D"
-       },
+    "What is the full meaning of PEP?": {
+        "options": ["a) Python element particles", "b) Prepared elected python", "c) Python pack project", "d) Python enhancement proposal"],
+        "answer": "d"
+  },
+    "What is the full meaning of Ram?": {
+      "options": ["a) Remedy after memory","b) Random acess memory","c) Release acess memory","d) Ready at memory"],
+      "answer":"b"
+  },   
+    "What is RAM?": {
+      "options": ["a) Media for coding","b) Computer disk","c) A storage medium for computers","d) A file"],
+      "answer":"c"
+  },
+    "What will you get if you use a variable name before a variable is assigned?": {
+      "options" : ["a) Function", "b) File", "c) String", "d) Error"],
+      "answer": "d"
+      
+    }
+}
 
-       {"question": "Computers have a hardware called?",
-       "options" : ("A) Ram", "B) Storage", "C) Code", "D) Hard disk"),
-       "answer":"A"
-       },
 
-]
+
 
 
 
