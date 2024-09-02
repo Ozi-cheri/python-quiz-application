447ceb6f57833e92d07b47c5db550da30d9f39ec
Sylveria Ozioma Ihemedu <silvernasa@myyahoo.com>
2024-09-02 17:36:01 +0000

    Add readmefile,modify run.py and correct spelling

--- run.py
+++ run.py
@@ -1,50 +1,56 @@
 
-
-print("Welcome to Python Quiz Application: ")
+"""Welcoming the player"""
+print("Welcome to Python Quiz Application:")
 player_name = input("Please enter your name:")
 print(f"Hello, {player_name}! If you are ready let's start the quiz.\n")
-
+"""  Define the quiz questions,options and correct answers using dictionary  """
 quiz = {
-    
-    "What is the full meaning of PEP?": {
-        "options": ["a) Python element particles", "b) Prepared elected python", "c) Python pack project", "d) Python enhancement proposal"],
-        "answer": "d"
-  },
-    "What is the full meaning of Ram?": {
-      "options": ["a) Remedy after memory","b) Random acess memory","c) Release acess memory","d) Ready at memory"],
-      "answer":"b"
-  },   
-    "What is RAM?": {
-      "options": ["a) Media for coding","b) Computer disk","c) A storage medium for computers","d) A file"],
-      "answer":"c"
-  },
+    "What is the full meaning of PEP?" : {
+        "options": ["a) Python element particles", "b) Prepared elected python" ,
+"c) Python pack project" , "d) Python enhancement proposal"] ,
+        "answer" : "d"
+  } ,
+    "What is the full meaning of Ram?" : {
+      "options" : ["a) Remedy after memory","b) Random acess memory" ,
+"c) Release acess memory" , "d) Ready at memory"] ,
+      "answer" : "b"
+  } ,   
+    "What is RAM?" : {
+      "options" : ["a) Media for coding" , "b) Computer disk" ,
+"c) A storage medium for computers" , "d) A file"] ,
+      "answer" : "c"
+  } ,
     "What will you get if you use a variable name before a variable is assigned?": {
-      "options" : ["a) Function", "b) File", "c) String", "d) Error"],
+      "options" : ["a) Function", "b) File" , "c) String" , "d) Error"] ,
       "answer": "d"
       
     }
 }
+"""Initialize score"""
 score = 0
-
-for question, details in quiz.items():
+"""  Iterate through the quiz questions  """
+for question , details in quiz.items() :
   print(question)
-  for option in details["options"]:
+  for option in details["options"] :
     print(option)
-    
-  answer = input("Please enter the letter of the correct answer:").lower()
-  if answer == details["answer"]:
+  """Get the player's answer"""  
+  answer = input("Please enter the letter of the correct answer:"). lower()
+  """Check if the answer is correct"""
+  if answer == details["answer"] :
     print("Correct!\n")
+    
     score += 1
-  else:
-     print(f"Wrong! The correct answer is {details['answer']}.\n")
-  print(f"Your final score is {score}/{len(quiz)}")
-  if score == len(quiz):
+  else :
+     print(f"Wrong! The correct answer is {details['answer']}. \n")
+  """Provide feedback based on score"""
+  print(f"Your score is {score}/{len(quiz)}")
+  if score == len(quiz) :
       
       if score == len(quiz):
        print("Excellent! Congratulations you got all the questions correct.")
-  elif score >= len(quiz) // 2:
-      print("Good job")
-  else:
+  elif score >= len(quiz) // 2 :
+      print("Good Job")
+  else :
     ("Good luck next time. Keep learning. Thank you for participating.")
     
 
